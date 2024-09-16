f9176e4fc3c0c81ff50325df88372289e05549f4
Sami Islam <islam.samipkr@gmail.com>
2024-09-15 21:01:01 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,9 +9,7 @@ terraform{
 }
 
 provider "azurerm"{
-  features{}
-  skip_provider_registration="true"
-  
+  features{}  
   subscription_id=var.subscription_id
   client_id=var.client_id
   client_secret=var.client_secret
