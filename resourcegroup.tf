7ec3700e29d67bda6e9a5c76acdaf27119a63a24
mta92 <95572494+mta92@users.noreply.github.com>
2025-01-31 19:28:48 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "mcit420zz5um2_" {
   name     = "mcit_resource_group_w24_3_"
   location = "canadacentral"
 }
-resource "azurerm_resource_group" "mcit420zz5um3_" {
+/*resource "azurerm_resource_group" "mcit420zz5um3_" {
   name     = "mcit_resource_group_w24_3_"
   location = "canadacentral"
-}
+}*/
