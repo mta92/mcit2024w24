c5a022fa8488ab222114b40ed68443ab18cc37b2
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-03 18:55:13 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,10 +1,3 @@
-/*resource "random_string" "storage_account_name" {
-  length = 16
-  special = false
-  upper   = false
+resource "azurerm_storage_account" "mta002233" {
+  name                     = "st-mta20232125"
 }
-
-resource "azurerm_storage_account" "example" {
-  name                     = "st-${random_string.storage_account_name.result}"
-  # ... rest of the configuration
-}*/
