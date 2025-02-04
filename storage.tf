e47c0b78c8004c1c080de59d3f6e8867c5a15c03
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-03 19:57:09 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,27 +1,3 @@
-# Create the Windows virtual machine
-resource "azurerm_windows_virtual_machine" "mta_win_vm" {
-  name                  = "mtavm" # 
-  resource_group_name   = azurerm_resource_group.rgmta.name
-  location              = azurerm_resource_group.rgmta.location
-  size                  = "Standard_DS1_v2" 
-/*  network_interface_ids = [azurerm_network_interface.example.id]*/
-
-  admin_username = "adminuser" 
-  admin_password = "TerraformPass" 
-
-  os_disk {
-    name                 = "osdisk1-example-vm"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-Datacenter" 
-    version   = "latest"
-  }
-
 # Create a virtual network
 resource "azurerm_virtual_network" "rgmta" {
   name                = "vnet-rgmta-linux-vm"
