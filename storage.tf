1e495f4119062653420ab5a11b1f8dcf25b6431d
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-03 19:14:45 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,6 +1,7 @@
 resource "azurerm_storage_account" "mcitmta" {
   name                     = "st-mta20232125"
-  resource_group_name      = mcit_resource_group_w24_
-  location                 = "canadacentral"
+  resource_group_name      = azurerm_resource_group.mcit_resource_group111.name
+  location                 = azurerm_resource_group.mcit_resource_group111.location
   account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
