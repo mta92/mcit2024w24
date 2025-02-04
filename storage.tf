9df87c1a2ebc27116cb673e9672ec3a79a690a68
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-03 19:50:28 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,8 +1,23 @@
-/*resource "azurerm_storage_account" "mcitmta" {
-  name                     = "mtastorage01"
-  resource_group_name      = azurerm_resource_group.mcit420zz5um_.name
-  location                 = azurerm_resource_group.mcit420zz5um_.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-*/
+# Create the Windows virtual machine
+resource "azurerm_windows_virtual_machine" "mta_win_vm" {
+  name                  = "mtavm" # 
+  resource_group_name   = azurerm_resource_group.rgmta.name
+  location              = azurerm_resource_group.rgmta.location
+  size                  = "Standard_DS1_v2" 
+/*  network_interface_ids = [azurerm_network_interface.example.id]*/
+
+  admin_username = "adminuser" 
+  admin_password = "TerraformPass" 
+
+  os_disk {
+    name                 = "osdisk1-example-vm"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter" 
+    version   = "latest"
+  }
