617b62296ff3fd8faab2361be4c5dbeed64163aa
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-05 19:31:13 -0500

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -6,7 +6,7 @@ resource "azurerm_web_application_firewall_policy" "waf_policy" {
 
 # Define the policy settings (at least one rule set is required)
   policy_settings {
-    mode                 = "Prevention" # Or "Detection"
+    mode                 = "Detection" 
     state                = "Enabled"
     request_body_check   = true # Example setting
     file_upload_limit_mb = 100  # Example setting
