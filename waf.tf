25f5bed98c9ed6dffa27c0c3b4a7814f863bb3ea
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-05 19:10:49 -0500

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -3,7 +3,7 @@ resource "azurerm_web_application_firewall_policy" "rgmta" {
   name                = "tatsa"  # The name you specified
   resource_group_name = azurerm_resource_group.rgmta.name
   location            = azurerm_resource_group.rgmta.location
-
+}
   # Define the policy settings (at least one rule set is required)
   policy_settings {
     mode                 = "Prevention" # Or "Detection"
