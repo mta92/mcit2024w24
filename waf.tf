5c709a3cf421e7a1f16b5430478dbc631a2454b8
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-05 19:08:11 -0500

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -1,5 +1,5 @@
 # Create the WAF Policy
-resource "azurerm_web_application_firewall_policy" "waf_policy" {
+resource "azurerm_web_application_firewall_policy" "rgmta" {
   name                = "tatsa"  # The name you specified
   resource_group_name = azurerm_resource_group.rgmta.name
   location            = azurerm_resource_group.rgmta.location
